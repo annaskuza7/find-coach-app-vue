1b0499efc7386f4711aad8d77d500bc8ef166098
annaskuza7 <anna.skuza7@gmail.com>
2022-02-23 10:03:20 +0100

    add Not Found page

---
+++ src/pages/NotFound.vue
@@ -0,0 +1,17 @@
+<template>
+  <section>
+    <BaseCard>
+      <h2>Page not found</h2>
+      <p>
+        This page could not be found - maybe check out all our
+        <router-link to="/coaches">coaches</router-link>
+      </p>
+    </BaseCard>
+  </section>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped></style>
